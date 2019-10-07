7b17cbbac970b8899f12f944ecd8c95777f83d40
Kevin Darcy <neoglitch822@yahoo.com>
2019-10-07 17:57:38 -0400

    altered spacing, borders and added ratings

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
 // This is making buttons = to the names and length of the array...
-var topics = ["dog", "cat", "bird", "badger", "lion", "hamster", "pengiun", "unicorn"];
+var topics = ["dog", "cat", "bird", "badger", "lion", "hamster", "pengiun", "pikachu"];
 
 function listStuff() {
   for (var i = 0; i < topics.length; i++) {
@@ -25,19 +25,28 @@ $("button").on("click", function() {
 
     .then(function(response) {
       var results = response.data;
+      console.log(response);
 
       for (var i = 0; i < results.length; i++) {
         // This makes an img tag and...
         var topicImage = $("<img>");
+        var pOne = $("<p>").text("Rating: " + results[i].rating);
 
         // Gives it the src attribtute with the reuslts from above and...
         console.log( results[i].images.fixed_height.url);
 
-        topicImage.attr("src", results[i].images.fixed_height.url);
+        topicImage.attr("src", results[i].images.fixed_height.url).addClass("m-1 border border-light");
+
         
         // Put it inside the empty div in the HTML...
-        topicImage.append($("#gifs-here"));
+       // topicImage.append($("#gifs-here"));
+        $("#gifs-here").append(topicImage);
+        $("#gifs-here").append(pOne);
+
 
       }
     });
 });
+
+///get search box to APPEND to the array, whatever you type in.
+/// 
