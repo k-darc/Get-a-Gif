382ed7e14a276b63a4c8574ae687dfe80343008a
Kevin Darcy <neoglitch822@yahoo.com>
2019-10-08 19:15:50 -0400

    ratings above image, again, but they line up horizontally now. Added additional fonts and CSS. Made site semi-responsive. No tablet scalling, but mobile and desktop is there. Images aren't resizing, but everything else is. CANNOT get images to pause.

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,13 @@
 // This is making buttons = to the names and length of the array...
 var topics = [
-  'dog',
-  'cat',
-  'bird',
-  'badger',
-  'lion',
-  'hamster',
-  'pengiun',
+  'dogs',
+  'cats',
+  'birds',
+  'badgers',
+  'lions',
+  'hamsters',
+  'pengiuns',
+  'chipmunks',
   'pikachu'
 ];
 function listStuff() {
@@ -19,9 +20,10 @@ function listStuff() {
     // DON'T   USE innerHTML +, use innterHTML = ... don't use inner HTML at all here
   }
 }
-listStuff(); // Should I move this down   ??????????
+listStuff();
+
 // have to bind click event to the document because some buttons are dynamically created after the DOM has been loaded
-$(document).on('click', 'button', function() {
+$("#buttons-here").on('click', 'button', function() {
   var animals = $(this).attr('id');
   console.log($(this).attr('id'));
   // grabbing the urls with the correct topic(animal)...
@@ -30,6 +32,7 @@ $(document).on('click', 'button', function() {
     animals +
     '&api_key=uz1widOpk7jjqzkeT0nG7jOcCJ230XZ7&limit=10';
   console.log(queryURL);
+
   $.ajax({
     url: queryURL,
     method: 'GET'
@@ -37,24 +40,34 @@ $(document).on('click', 'button', function() {
     var results = response.data;
     console.log(response);
     for (var i = 0; i < results.length; i++) {
+
       // This makes an img tag and...
       var topicImage = $('<img>');
       var pOne = $('<p>').text('Rating: ' + results[i].rating);
+      var gifDiv = $("<div>");
+
+
       // Gives it the src attribtute with the reuslts from above and...
-      console.log(results[i].images.fixed_height.url);
+      console.log(results[i].images.fixed_height_still.url);
       topicImage
-        .attr('src', results[i].images.fixed_height.url)
+        .attr('src', results[i].images.fixed_height_still.url)
         .addClass('border border-light');
+      topicImage.addClass("giffy");
+
+      topicImage.attr("data-state", "still");
+      topicImage.attr("data-animate", results[i].images.fixed_height.url);
+      topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+
       // Put it inside the empty div in the HTML...
-      $('#gifs-here').prepend(topicImage);
-      $('#gifs-here').prepend(pOne);
+      gifDiv.prepend(pOne);
+      gifDiv.append(topicImage);
+      $('#gifs-here').prepend(gifDiv);
     }
   });
 });
 
-// Adding things to the button array via the ADD input box
+
 $('#add-movie').on('click', function(event) {
-  //This is supposed to empty the div and apply the new ones but it just duplicates!
   event.preventDefault();
   var movie = $('#movie-input')
     .val()
@@ -63,6 +76,18 @@ $('#add-movie').on('click', function(event) {
   console.log('you typed ' + movie);
   listStuff();
 });
-$(document).on('click', '.add-movie', topics); //make "topics" itself or "listStuff", does the same thing...
+$(document).on('click', '.add-movie', topics);
 
 // ---------------- Pausing Gifts ---------
+
+$(".gif").on("click", '.giffy', function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
